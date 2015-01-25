3e074b14f52fc4c8aeea375a3a1ef8f61006ed85
Luke Penn-Hall <luke.c.pennhall@gmail.com>
2015-01-25 15:36:38 -0500

    this is the updated code for the matri assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This function shoud generate a matrix, find its inverse, and then cache
+## the result
 
-## Write a short comment describing this function
+## This function creates the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  ##Making and caching the Matrix and the placeholdler for its inverse
+  x<<- x
+  i<<- NULL
 }
 
 
-## Write a short comment describing this function
+## This is the function which caches the inverse
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Checking to see if the inverse has already been found
+  if(!is.null(i)) {
+    message("getting cached data")
+    i
+  } 
+  
+  ##calcutating the inverse if not already present
+  else {
+    data <- x
+    i <- solve(data)
+    i<<-i
+    
+    ##Printing the Inverse
+    i
+  }
 }
